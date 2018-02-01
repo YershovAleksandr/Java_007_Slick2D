01b993a4b8d51dd958463f812505ed47d7842bf4
hp <namstudionsk@gmail.com>
2018-02-02 04:02:15 +0700

    Add all files

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -1,8 +1,55 @@
 package com.nam;
 
-public class Main {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Main extends BasicGame
+{
+    public Main(String gamename)
+    {
+        super(gamename);
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {}
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {}
+
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException
+    {
+        g.drawString("Howdy!", 10, 10);
+        g.drawString("Hello world Motherfucka!!!!", 100, 100);
+        g.drawString("Пидор сука!!!!", 100, 200);
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new Main("Simple Slick Game"));
+            appgc.setDisplayMode(640, 480, false);
+            appgc.start();
+        }
+        catch (SlickException ex)
+        {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+/*public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
-}
+}*/
+
+//package simpleslickgame;
